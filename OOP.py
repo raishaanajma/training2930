ccba4af24f248721c74e751848fc7e7a60338571
raishaanajma <raishanajmaa@gmail.com>
2025-04-25 11:01:41 +0700

    hands on

--- OOP.py
+++ OOP.py
@@ -3,15 +3,15 @@ import rapihin
 rapihin.header(1, "Object Oriented Programming")
 class Mobil:
     def __init__(self, merk, warna):
-        self.merk = merk      # atribut objek
-        self.warna = warna    # atribut objek
+        self.merk = merk      #atribut objek
+        self.warna = warna    #atribut objek
 
     def info(self):
         print(f"Mobil {self.merk} berwarna {self.warna}")
 
-# Membuat objek dari kelas Mobil
+#membuat objek dari kelas Mobil
 mobil1 = Mobil("Toyota", "Merah")
-mobil1.info()  # Output: Mobil Toyota berwarna Merah
+mobil1.info()  #output: Mobil Toyota berwarna Merah
 
 rapihin.divider()
 
