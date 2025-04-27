ec386bd4fbd41095bc80d4716fc028bb301e941a
Raisha A. <raishanajmaa@gmail.com>
2025-04-28 00:26:49 +0700

    Update data_structure-math_operations.py

--- data_structure-math_operations.py
+++ data_structure-math_operations.py
@@ -1,6 +1,36 @@
 #String
 string1 = "ABCdef"
 
-print(string1[1]) #index 1 -> urutan ke-2
+#String operation
+#Concatination -> +
 sum_string1 = string1 + string1[0]
 print(string1) #output ABCdefA
+#Q1
+print(string1 * 2) #--> ?
+
+#Indexing
+print(string1[0]) 
+print(string1[2])
+print(string1[-3])
+
+#Q2
+print(string1[6]) #--> ?
+
+#Slicing
+
+#Update/Replace
+
+#Count Length
+
+#Remove Whitespace
+
+#"in" Keyword
+
+#String Comparison
+
+
+#List
+list1 = [10, 20, "UIAUU", "50", True]
+
+#Indexing
+#Multiplication
